bd45d5d2ebd0c2fffd29b503b6910715129dd834
santhosh-v11 <santhosh592002@gmail.com>
2025-07-14 19:45:08 +0530

    addedproject

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-// import logo from './logo.svg';
+
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
@@ -32,10 +9,10 @@ import Project from './components/Project'
 import Contact from './components/contact'
 import Footer from './components/Footer'
 
-
 const App = () => {
   return (
     <div>
+
       <Navbar></Navbar>
       <Header></Header>
       <About></About>
